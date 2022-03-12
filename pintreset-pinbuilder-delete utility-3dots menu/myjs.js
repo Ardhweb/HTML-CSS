447173a08d35f12b4d15dbeb42d86d187899332d
Rahul Mahawar <68722733+Ardhweb@users.noreply.github.com>
2022-03-12 23:47:32 +0530

    pinclonecommit2

---
+++ pintreset-pinbuilder-delete utility-3dots menu/myjs.js
@@ -0,0 +1,9 @@
+function menu21utlity() {
+    var xclic = document.getElementById("menu21" , "btnmenu21svg");
+  
+    if (xclic .style.display === "block") {
+      xclic.style.display = "none";
+    } else {
+      xclic.style.display = "block";
+    }
+  }
